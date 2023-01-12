331764b36305a823f30af0085ddf3befec140e4d
HDDen <denis@osagohelper.ru>
2023-01-12 10:00:33 +0300

    Updatet style, added js for example mobile spoiler

---
+++ htmlsitemap/example.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function(){
+    /**
+     * Sitemap menu spoilers
+     */
+    var sitemap_spoilers = document.querySelectorAll('.sitemap-menu > ul > li');
+    if (sitemap_spoilers.length) {
+        for (var i = 0; i < sitemap_spoilers.length; i++) {
+            sitemap_spoilers[i].addEventListener('click', function (e) {
+                if (e.target === e.currentTarget) {
+                    e.currentTarget.classList.toggle('active');
+                }
+            });
+        }
+    }
+});
