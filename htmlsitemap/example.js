cc99a5defe095cd42529a95dbe44483f55c0ecca
HDDen <denis@osagohelper.ru>
2023-01-12 10:02:48 +0300

    Update example.js

--- htmlsitemap/example.js
+++ htmlsitemap/example.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
     /**
      * Sitemap menu spoilers
      */
-    var sitemap_spoilers = document.querySelectorAll('.sitemap-menu > ul > li');
+    var sitemap_spoilers = document.querySelectorAll('.htmlsitemap > ul > li');
     if (sitemap_spoilers.length) {
         for (var i = 0; i < sitemap_spoilers.length; i++) {
             sitemap_spoilers[i].addEventListener('click', function (e) {
